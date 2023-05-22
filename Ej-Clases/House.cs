6094c9249813dec357da5f12e319fc363d43a327
sofimerlos <sofiamerlos@gmail.com>
2023-05-22 02:56:02 -0300

    A ver ahorita

---
+++ Ej-Clases/House.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+1- Crear una clase casa que tenga atributos cantidad de puertas, 
+ventanas, color exterior, material, altura y sus metodos 
+para acceder y especificar estos atributos.
+*/
+
+namespace Ej_Clases
+{
+    internal class House
+    {
+        int doorsQuantity;
+        int windowsQuantity;
+        string color;
+        string material;
+        double height;
+
+        public void SetDoorsQuantity(int doorsQuantity)
+        {
+            this.doorsQuantity = doorsQuantity;
+        }
+
+        public void SetWindowsQuantity(int windowsQuantity)
+        {
+            this.windowsQuantity = windowsQuantity;
+        }
+
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+
+        public void SetMaterial(string material)
+        {
+            this.material = material;
+        }
+
+        public void SetHeight(double height)
+        {
+            this.height = height;
+        }
+
+
+        public int GetDoorsQuantity()
+        {
+            return this.doorsQuantity;
+        }
+
+        public int GetWindowsQuantity()
+        {
+            return this.windowsQuantity;
+        }
+
+        public string GetColor()
+        {
+            return this.color;
+        }
+
+        public string GetMaterial()
+        {
+            return this.material;
+        }
+
+        public double GetHeight()
+        {
+            return this.height;
+        }
+
+
+        public void AddInfoHouse()
+        {
+            Console.Write("Ingrese cantidad de Puertas: ");
+            SetDoorsQuantity(int.Parse(Console.ReadLine()));
+
+            Console.Write("Cantidad de Ventanas: ");
+            SetWindowsQuantity(int.Parse(Console.ReadLine()));
+
+            Console.Write("Ingrese el color: ");
+            SetColor(Console.ReadLine());
+
+            Console.Write("Ingrese el Material: ");
+            SetMaterial(Console.ReadLine());
+
+            Console.Write("Ingrese la altura:");
+            SetHeight(double.Parse(Console.ReadLine()));
+        }
+
+        public void SeeInfoHouse()
+        {
+            Console.WriteLine("\nPuertas:" + GetDoorsQuantity() + " Ventanas:" + GetWindowsQuantity() + " Color:" + GetColor() + " Material:" + GetMaterial() + " Altura:" + GetHeight());
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Ej-Clases/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+1- Crear una clase casa que tenga atributos cantidad de puertas, 
+ventanas, color exterior, material, altura y sus metodos 
+para acceder y especificar estos atributos.
+*/
+
+namespace Ej_Clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House house = new House();
+            //house.SetDoorsQuantity(5);
+            //house.SetWindowsQuantity(2);
+            //house.SetColor("Blanco");
+            //house.SetMaterial("Cemento");
+            //house.SetHeight(3.55);
+
+            /*
+            Console.Write("Ingrese cantidad de Puertas: ");
+            house.SetDoorsQuantity(int.Parse(Console.ReadLine()));
+            
+            Console.Write("Cantidad de Ventanas: ");
+            house.SetWindowsQuantity(int.Parse(Console.ReadLine()));
+            
+            Console.Write("Ingrese el color: ");
+            house.SetColor(Console.ReadLine());
+
+            Console.Write("Ingrese el Material: ");
+            house.SetMaterial(Console.ReadLine());
+
+            Console.Write("Ingrese la altura:");
+            house.SetHeight(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Puertas:" + house.GetDoorsQuantity()+ " Ventanas:"+ house.GetWindowsQuantity()+ " Color:"+ house.GetColor()+ " Material:"+ house.GetMaterial()+ " Altura:"+house.GetHeight());
+            Console.ReadLine();
+            */
+
+            house.AddInfoHouse();
+            house.SeeInfoHouse();
+        }
+    }
+}
